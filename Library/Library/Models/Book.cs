a9a1d2a84e580707177bb048afb298abf3668df0
rnmisrahi <rnmisrahi@gmail.com>
2015-12-30 17:59:19 +0200

    Library project

---
+++ Library/Library/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string Editor { get; set; }
+        public string Audience { get; set; }
+        public string Category { get; set; }
+        public int Copies { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Library/Library/Models/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public static class BookRepository
+    {
+        public static List<Book> Books = new List<Book>
+        {
+            new Book {BookId="ABCDEF",Audience="Teen",Category="History",Editor="ABC",Title="Grapes of Wrath", Copies=30, IsActive=true },
+            new Book{BookId="XYZABC",Title="The Stand",Audience="Adults",Category="Fiction",Editor="Random House",Copies=75,IsActive=true }
+        };
+    }
+}
